297a6aee8512a7033eadb4c9190bd902ada78681
AkashTiwari99 <95995496+AkashTiwari99@users.noreply.github.com>
2026-07-17 13:58:11 +0530

    ok

--- debug_git.bat
+++
@@ -1,7 +0,0 @@
-  @echo off
-echo HEAD: > git_state.txt
-git rev-parse HEAD >> git_state.txt
-echo ORIGIN/MAIN: >> git_state.txt
-git rev-parse origin/main >> git_state.txt
-echo STATUS: >> git_state.txt
-git status >> git_state.txt
